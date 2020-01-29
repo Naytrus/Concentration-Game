363f3f3d37c9f5607d94be610d728d40d9cd3263
den <deanstreets@protonmail.com>
2020-01-28 19:34:39 -0800

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Dean on 1/28/20.
+//  Copyright © 2020 Naytrus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
